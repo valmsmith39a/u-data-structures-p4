0ef7d010c0ad9af15720a7c2ff3c5dd6074f34dd
valmsmith39a <valmsmith39a@gmail.com>
2020-10-07 22:29:54 -0700

    first try to traverse to next intersection

--- student_code.py
+++ student_code.py
@@ -8,31 +8,38 @@ def shortest_path(M, start, goal):
     start_coord = intersections[start]
     goal_coord = intersections[goal]
 
-    connected_roads = roads[start]
-    connected_road_coords = []
+    current_intersection = start
     current_coord = start_coord
+    count = 0
+    while(current_intersection != goal and count < 50):
+        connected_roads = roads[current_intersection]
+        connected_road_coords = []
+        f_total_estimated_cost = 0
+        f_least = [0, 0]
 
-    f_total_estimated_cost = 0
-    f_least = [0, 0]
+        for connected_road in connected_roads:
+            connected_road_coords.append(
+                [intersections[connected_road], connected_road])
 
-    for connected_road in connected_roads:
-        connected_road_coords.append(
-            [intersections[connected_road], connected_road])
+        count += 1
 
-    for idx, connected_road_coord in enumerate(connected_road_coords):
-        g_path_cost = math.sqrt((current_coord[0] - connected_road_coord[0][0]) ** 2 + (
-            current_coord[1] - connected_road_coord[0][1]) ** 2)
-        h_estimated_path_cost = math.sqrt(
-            connected_road_coord[0][0] - goal_coord[0] ** 2 + connected_road_coord[0][1] - goal_coord[1] ** 2)
+        for idx, connected_road_coord in enumerate(connected_road_coords):
+            result = (current_coord[0] - connected_road_coord[0][0]) ** 2 + \
+                (current_coord[1] - connected_road_coord[0][1]) ** 2
+            g_path_cost = math.sqrt(result)
 
-        f = g_path_cost + h_estimated_path_cost
+            result2 = (connected_road_coord[0][0] - goal_coord[0]) ** 2 + (
+                connected_road_coord[0][1] - goal_coord[1]) ** 2
+            h_estimated_path_cost = math.sqrt(result2)
 
-        if idx is 0:
-            f_least = [f, connected_road_coord]
+            f = g_path_cost + h_estimated_path_cost
 
-        if f < f_least[0]:
-            f_least = [f, connected_road_coord]
+            if idx is 0:
+                f_least = [f, connected_road_coord]
 
-    # TODO: Get to the next coordinate based on the least estimated total path cost
+            if f < f_least[0]:
+                f_least = [f, connected_road_coord]
+
+        current_intersection = f_least[1][1]
 
     return
